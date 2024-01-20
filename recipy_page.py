44e0afe6ea6e27b3223551dc03d99813fe9f8ab3
vhuber-sama <117632632+vhuber-sama@users.noreply.github.com>
2024-01-20 17:29:21 +0100

    ajout de l'UI (sans affichage des recettes)

--- recipy_page.py
+++ recipy_page.py
@@ -26,6 +26,39 @@ class RecipyPage:
         self.screen.resizable(0, 0)
         self.screen.configure(background=self.bg_color)
 
+
+        self.screen.rowconfigure(0,weight=0)
+        self.screen.rowconfigure(1,weight = 8)
+        self.screen.rowconfigure(2,weight=1)
+
+        self.screen.columnconfigure(0,weight=1)
+        self.screen.columnconfigure(1,weight=6)
+
+        self.header = Frame(self.screen, background=self.main_color)
+        self.main_widgets.append(self.header)
+        self.header.grid(row=0, column=0, columnspan=4)
+
+        self.lbl_logo = ttk.Label(self.screen, text="JustCook")
+        self.main_widgets.append(self.lbl_logo)
+        self.lbl_logo.grid(row=0, column=0, columnspan=4, sticky=NSEW)
+        self.lbl_logo.config(background=self.main_color, padding=(100, 0), font=("Z003", 45))
+
+        self.recette_cherchee = ""
+        self.barre_recherche = Entry(self.screen, textvariable=self.recette_cherchee, background="white")
+        self.barre_recherche.grid(row=0, column=2, columnspan=2, padx=250, sticky=EW)
+
+        self.side_panel = Frame(self.screen,background=self.main_color)
+        self.main_widgets.append(self.side_panel)
+        self.side_panel.grid(column=0,row=1,sticky=NSEW,rowspan=2)
+
+
+        self.btn_footer = Button(self.screen, text="Exit", bg=self.main_color, command=self.screen.destroy)
+        self.main_widgets.append(self.btn_footer)
+        self.btn_footer.grid(column=2, row=5, sticky=NSEW, columnspan=2)
+
+        self.btn_night = Button(self.screen, text='Nightmode', command=self.screen_mode_update,background=self.main_color)
+        self.main_widgets.append(self.btn_night)
+        self.btn_night.grid(column=0, row=5, columnspan=2, sticky=NSEW)
         self.screen.mainloop()
 
     def nightmode(self):
@@ -59,4 +92,4 @@ class RecipyPage:
         infos = requestDB.request.get_recette_info(self.id)
 
 
-recipy = RecipyPage(0)
+#recipy = RecipyPage(0)
