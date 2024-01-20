50d2b89685233040146a5c386c746a86b3184237
vhuber-sama <117632632+vhuber-sama@users.noreply.github.com>
2024-01-20 18:21:49 +0100

    Update cat_page.py

--- cat_page.py
+++ cat_page.py
@@ -36,7 +36,9 @@ class CategoryPage:
         self.screen.columnconfigure(2, weight=3)
         self.screen.columnconfigure(3, weight=3)
 
-        self.create_rows()
+        self.screen.rowconfigure(0,weight=0)
+        self.screen.rowconfigure(1,weight=8)
+        self.screen.rowconfigure(2,weight=0)
 
         self.header = Frame(self.screen, background=self.main_color)
         self.main_widgets.append(self.header)
@@ -50,7 +52,28 @@ class CategoryPage:
         self.recette_cherchee = ""
         self.barre_recherche = Entry(self.screen, textvariable=self.recette_cherchee, background="white")
         self.barre_recherche.grid(row=0, column=2, columnspan=2, padx=250, sticky=EW)
+
+
+        self.recette_frame = Frame(self.screen,background="red")
+        self.background_widgets.append(self.recette_frame)
+        self.recette_frame.grid(row=1,column=0,columnspan=4,sticky=NSEW)
+        self.recette_frame.columnconfigure(0, weight=1)  # somehow, si on met les poids à 1 partout ils sont pas tous égaux
+        self.recette_frame.columnconfigure(1, weight=1)
+        self.recette_frame.columnconfigure(2, weight=1)
+        self.recette_frame.columnconfigure(3, weight=1)
+
+        self.btn_footer = Button(self.screen, text="Exit", bg=self.main_color, command=self.screen.destroy)
+        self.main_widgets.append(self.btn_footer)
+        self.btn_footer.grid(column=2, row=5, sticky=NSEW, columnspan=2)
+
+        self.btn_night = Button(self.screen, text='Nightmode', command=self.screen_mode_update,background=self.main_color)
+        self.main_widgets.append(self.btn_night)
+        self.btn_night.grid(column=0, row=5, columnspan=2, sticky=NSEW)
+
+        self.create_rows()
         
+
+
         self.screen.mainloop()
 
     def nightmode(self):
@@ -82,7 +105,7 @@ class CategoryPage:
         Fonction qui crée le bon nombre de lignes selon le nombre de recettes et crée un bouton au bon
         emplacement par recette
         """
-        recettes = requestDB.request.get_recettes("recettes", "nom, id", self.category)  # appel la fonction de Matteo
+        recettes = requestDB.request.get_recettes("recettes", "nom, id, image", self.category)  # appel la fonction de Matteo
         print(recettes)
         nb_lignes = len(recettes) / 4  # on affiche 4 recettes
 
@@ -94,11 +117,14 @@ class CategoryPage:
             print(nb_lignes)
 
             for i in range(nb_lignes):
-                self.screen.rowconfigure(i)
+                self.recette_frame.rowconfigure(i)
                 for j in range(4):
                     if 2*i+j < len(recettes):
-                        recette = Button(self.screen, text=recettes[2*i+j][0], command=lambda m=recettes[2*i+j][1]: self.go_to_selected_recipy(m))
-                        recette.grid(column=j, row=i+1)
+                        image_recette = PhotoImage(file=recettes[2*i+j][2])
+                        #image_bouton = Button(self.recette_frame,image=image_recette)
+                        #image_bouton.grid(column=j,row=i+1)
+                        recette = Button(self.recette_frame, text=recettes[2*i+j][0],image = image_recette,compound=TOP, command=lambda m=recettes[2*i+j][1]: self.go_to_selected_recipy(m))
+                        recette.grid(column=j, row=i+1,pady=15)
 
     def go_to_selected_recipy(self, recette_id):
         """
