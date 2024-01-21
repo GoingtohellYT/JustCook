165f1c35f36ea1c094cbea66ce86cfb93fc5f43b
MATT-07 <117632549+MATT-07@users.noreply.github.com>
2024-01-21 14:50:09 +0100

    Update requestDB.py

--- requestDB.py
+++ requestDB.py
@@ -37,6 +37,17 @@ class Request:
         self.c.execute(f"SELECT * FROM recettes WHERE id = {recette_id}")
         return self.c.fetchall()[0]
 
+    def get(self, attribut, table,condition1,  condition2):
+        """
+        Fonction qui permet d'acceder à la base de données en fonction d'une condition qui dépend de l'attribut
+        :param attribut:
+        :param table:
+        :param condition:
+        :return:
+        """
+        self.c.execute(f"SELECT {attribut} FROM {table} WHERE {condition1} = {condition2}")
+        return self.c.fetchall()
+
     # fonction login(email, mdp)
     def login(self, email, mdp):
         """
