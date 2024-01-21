7d93ed41f582dd5e640c9d8ce12df36065f35e6a
Goingtohell <78041803+GoingtohellYT@users.noreply.github.com>
2024-01-21 12:19:02 +0100

    Magie noire et ajout dans la DB

--- cat_page.py
+++ cat_page.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import ttk
 import requestDB
 import recipy_page
+from PIL import Image, ImageTk
 
 
 # from requestDB import ...
@@ -24,8 +25,8 @@ class CategoryPage:
 
         self.main_widgets = []
         self.background_widgets = []
-##=================================================================##L'ECRAN##=================================================================##
-        self.screen = Tk()
+        ##=================================================================##L'ECRAN##=================================================================##
+        self.screen = Toplevel()
         self.screen.title("JustCook")
         self.screen.geometry("1280x720")
         self.screen.resizable(0, 0)
@@ -36,13 +37,13 @@ class CategoryPage:
         self.screen.columnconfigure(2, weight=3)
         self.screen.columnconfigure(3, weight=3)
 
-        self.screen.rowconfigure(0,weight=0)
-        self.screen.rowconfigure(1,weight=8)
-        self.screen.rowconfigure(2,weight=0)
+        self.screen.rowconfigure(0, weight=0)
+        self.screen.rowconfigure(1, weight=8)
+        self.screen.rowconfigure(2, weight=0)
 
-##=================================================================##LES WIDGETS##=================================================================##
+        ##=================================================================##LES WIDGETS##=================================================================##
 
-#________________Le header________________#
+        # ________________Le header________________#
 
         self.header = Frame(self.screen, background=self.main_color)
         self.main_widgets.append(self.header)
@@ -57,12 +58,13 @@ class CategoryPage:
         self.barre_recherche = Entry(self.screen, textvariable=self.recette_cherchee, background="white")
         self.barre_recherche.grid(row=0, column=2, columnspan=2, padx=250, sticky=EW)
 
-#________________Contenu principal de la fenêtre________________#
-        
-        self.recette_frame = Frame(self.screen,background=self.bg_color)
+        # ________________Contenu principal de la fenêtre________________#
+
+        self.recette_frame = Frame(self.screen, background=self.bg_color)
         self.background_widgets.append(self.recette_frame)
-        self.recette_frame.grid(row=1,column=0,columnspan=4,sticky=NSEW)
-        self.recette_frame.columnconfigure(0, weight=1)  # somehow, si on met les poids à 1 partout ils sont pas tous égaux
+        self.recette_frame.grid(row=1, column=0, columnspan=4, sticky=NSEW)
+        self.recette_frame.columnconfigure(0,
+                                           weight=1)  # somehow, si on met les poids à 1 partout ils sont pas tous égaux
         self.recette_frame.columnconfigure(1, weight=1)
         self.recette_frame.columnconfigure(2, weight=1)
         self.recette_frame.columnconfigure(3, weight=1)
@@ -70,13 +72,14 @@ class CategoryPage:
         self.test_image = PhotoImage(file="img_recettes/pates_au_beurre.png")
         self.test_btn = Button(self.recette_frame,text="pâtes au beurre",image=self.test_image,compound=TOP,command=lambda m=0: self.go_to_selected_recipy(m)).grid(row=1,column=1)
         """
-#________________Le footer________________#
+        # ________________Le footer________________#
 
         self.btn_footer = Button(self.screen, text="Exit", bg=self.main_color, command=self.screen.destroy)
         self.main_widgets.append(self.btn_footer)
         self.btn_footer.grid(column=2, row=5, sticky=NSEW, columnspan=2)
 
-        self.btn_night = Button(self.screen, text='Nightmode', command=self.screen_mode_update,background=self.main_color)
+        self.btn_night = Button(self.screen, text='Nightmode', command=self.screen_mode_update,
+                                background=self.main_color)
         self.main_widgets.append(self.btn_night)
         self.btn_night.grid(column=0, row=5, columnspan=2, sticky=NSEW)
 
@@ -84,7 +87,7 @@ class CategoryPage:
 
         self.screen.mainloop()
 
-##=================================================================##LES FONCTIONS##=================================================================##
+    ##=================================================================##LES FONCTIONS##=================================================================##
 
     def nightmode(self):
         if not self.is_night_mode:
@@ -115,7 +118,8 @@ class CategoryPage:
         Fonction qui crée le bon nombre de lignes selon le nombre de recettes et crée un bouton au bon
         emplacement par recette
         """
-        recettes = requestDB.request.get_recettes("recettes", "nom, id, image", self.category)  # appel la fonction de Matteo
+        recettes = requestDB.request.get_recettes("recettes", "nom, id, image",
+                                                  self.category)  # appel la fonction de Matteo
         print(recettes)
         nb_lignes = len(recettes) / 4  # on affiche 4 recettes
 
@@ -123,20 +127,36 @@ class CategoryPage:
             if int(nb_lignes) != float(nb_lignes):
                 nb_lignes = int(nb_lignes)
                 nb_lignes += 1
+            else:
+                nb_lignes = int(nb_lignes)
 
-            print(nb_lignes)
+            # print(nb_lignes)
+
+            index = 0
+            images = list()
+            btns = list()
+            indexes = list()
 
             for i in range(nb_lignes):
                 self.recette_frame.rowconfigure(i)
                 for j in range(4):
-                    if 2*i+j < len(recettes):
-                        self.image_recette = PhotoImage(file=recettes[2*i+j][2])
-                        print(self.image_recette)
-                        self.image_bouton = ttk.Button(self.recette_frame,text="pâtes au beurre",image=self.image_recette,compound=TOP,command=lambda m=recettes[2*i+j][1]: self.go_to_selected_recipy(m)).grid(row=1,column=1)
-                        print(self.image_bouton)
-                        self.image_bouton.grid(row=2*i+j,column=j)
-                        #recette = Button(self.recette_frame,text=recettes[2*i+j][0], command=lambda m=recettes[2*i+j][1]: self.go_to_selected_recipy(m))
-                        #recette.grid(column=j, row=i+2,pady=(0,15),padx=15,sticky=NSEW)
+                    if index < len(recettes):
+                        print(index)
+                        image_recette = Image.open(recettes[index][2])
+                        image_recette = ImageTk.PhotoImage(image_recette.resize((250, 250)))
+                        setattr(CategoryPage, "image_recette_"+str(index), image_recette)
+                        images.append(getattr(CategoryPage, "image_recette_"+str(index)))
+                        # print(self.image_recette)
+                        btns.append((ttk.Button(self.recette_frame, text=recettes[index][0],
+                                                       image=images[index], compound=TOP,
+                                                       command=lambda m=recettes[index][1]: self.go_to_selected_recipy(
+                                                           m))))
+                        indexes.append((i, j))
+                        #self.image_bouton.grid(row=i, column=j)
+                        index += 1
+
+            for i in range(len(btns)):
+                btns[i].grid(row=indexes[i][0], column=indexes[i][1])
 
     def go_to_selected_recipy(self, recette_id):
         """
@@ -144,4 +164,4 @@ class CategoryPage:
         """
         recipy_page.RecipyPage(recette_id)
 
-#test = CategoryPage("Pâtes")
+# test = CategoryPage("Pâtes")
