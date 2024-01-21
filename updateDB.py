7b14b6b1507a23b816aac025e279eee6126a8a07
MATT-07 <117632549+MATT-07@users.noreply.github.com>
2024-01-21 12:22:03 +0100

    Update updateDB.py

--- updateDB.py
+++ updateDB.py
@@ -1,264 +1,78 @@
-import sqlite3
+from updateDB import check_user, set_current_user
 import argon2
-
-current_user = None
-
-
-# ------ Fonctions de vérification dans la DB ------ #
-def check_user(email):
-    """
-    Fonction qui vérifie si l'utilisateur existe
-
-    Retourne :
-        True ou False selon si l'utilisateur est dans la DB
-    Pré-conditions :
-        email est un email valide
-    Post-conditions :
-        un booléen est renvoyé et la DB n'est pas modifiée
-    """
-    assert "@" in email and "." in email, "l'email n'est pas valide"
-
-    connexion = sqlite3.connect('websiteDB.db')
-    c = connexion.cursor()
-
-    c.execute(f"""
-    SELECT email FROM users
-    WHERE email LIKE "{email}";
-    """)
-
-    result = c.fetchall()
-    c.close()
-    return len(result) == 1
+import sqlite3
 
 
-def set_current_user(email):
-    """
-    Sets the current user to the specified email
-    """
-    assert check_user(email), "L'utilisateur n'existe pas"
+class Request:
 
-    global current_user
-    current_user = email
+    def __init__(self, nom_db):
+        # créer un pont avec la base de données pour pouvoir faire de requête SQL
+        self.connexion = sqlite3.connect(nom_db)
+        self.c = self.connexion.cursor()
 
-
-def check_recette(id_recette):
-    """
-    Fonction qui vérifie si la recette est présente dans la DB
-
-    Retourne :
-        True ou False selon si la recette est dans la DB
-    Pré-conditions :
-        id_recette est un integer supérieur ou égal à 0
-    Post-conditions :
-        Un booléen est renvoyé et la DB n'est pas modifiée
-    """
-    assert type(id_recette) is int and id_recette >= 0
-
-    connexion = sqlite3.connect('websiteDB.db')
-    c = connexion.cursor()
-
-    c.execute(f"""
-    SELECT id FROM recettes
-    WHERE id LIKE {id_recette};
-    """)
-
-    result = c.fetchall()
-    c.close()
-    return len(result) == 1
-
-
-def check_favoris(email, id_recette):
-    """
-    Fonction qui vérifie si une recette fait partie des favoris d'un utilisateur
-
-    Retourne :
-        True ou False selon si la recette fait partie des favoris de l'utilisateur
-    Pré-conditions :
-        id_recette est l'identifiant d'une recette existante
-        email est l'email d'un utilisateur existant
-    Post-conditions :
-        Un booléen est renvoyé et la DB n'est pas modifiée
-    """
-    assert check_recette(id_recette), "La recette n'existe pas"
-    assert check_user(email), "L'utilisateur n'existe pas"
-
-    connexion = sqlite3.connect("websiteDB.db")
-    c = connexion.cursor()
-
-    c.execute(f"""
-    SELECT email FROM favoris
-    WHERE id_recette LIKE {id_recette};
-    """)
-
-    result = c.fetchall()
-    c.close()
-
-    return email in result[0]
-
-
-# ------ Fonctions d'ajout dans la DB ------ #
-def add_user(pseudo, mdp, email):
-    """
-    Fonction qui permet d'ajouter un utilisateur à la db
-
-    Retourne :
-        Rien
-    Pré-conditions :
-        pseudo est du type string
-        mdp est du type string
-        email est du type string
-    Post-conditions :
-        un nouvel utilisateur est ajouté à la db
-    """
-    assert type(pseudo) is str, "pseudo is not str"
-    assert type(mdp) is str, "mdp is not str"
-    assert type(email) is str, "email is not str"
-    assert "@" in email and "." in email, "email not valid"
-
-    ph = argon2.PasswordHasher()
-    hashed_mdp = ph.hash(mdp)
-    print(hashed_mdp)
-    connexion = sqlite3.connect('websiteDB.db')
-    c = connexion.cursor()
-
-    c.execute(f"""
-    INSERT INTO users
-    VALUES ("{pseudo}", "{str(hashed_mdp)}", "{email}");
-    """)
-
-    connexion.commit()
-
-    c.execute(f"""
-    INSERT INTO user_settings
-    VALUES ({0}, {0}, "{email}")
-    """)
-
-    connexion.commit()
-
-    c.close()
-
-    global current_user
-    current_user = email
-
-
-def add_recette(nom, ingredients, recette, categories, image, submitted_by):
-    """
-    Fonction qui ajoute une recette à la db
-
-    Retourne :
-        Rien
-    Pré-conditions :
-        tous les paramètres sont du type string (les ingredients sont séparés par des ';')
-    """
-    assert type(nom) is str
-    assert type(ingredients) is str
-    assert type(categories) is str
-    assert check_user(submitted_by)
-
-    connexion = sqlite3.connect('websiteDB.db')
-    c = connexion.cursor()
-
-    c.execute("""
-    SELECT MAX(id) FROM recettes;
-    """)
-
-    result = c.fetchall()
-    # print(result)
-    if result[0][0] is not None:  # S'il y a déjà des recettes dans la DB
-        last_id = result[0][0]
-
-        c.execute(f"""
-        INSERT INTO recettes
-        values (?, ?, ?, ?, ?, ?, ?);
-        """, (last_id + 1, nom, ingredients, recette, categories, image, submitted_by))
-    else:  # Si la table recettes est vide
-        c.execute(f"""
-        INSERT INTO recettes
-        VALUES ({0}, "{nom}", "{ingredients}", "{recette}", "{categories}", "{image}", "{submitted_by}");
-        """)
-
-    connexion.commit()
-
-    c.close()
-
-
-def add_favori(id_recette):
-    """
-    Fonction qui ajoute un favori à l'utilisateur actuel
-
-    Retourne :
-        Rien
-    Pré-conditions :
-        id_recette est un integer qui correspond à l'id d'une recette dans la table recettes
-    Post-conditions :
-        La recette est ajoutée à l'utilisateur actuel
-    """
-    assert type(id_recette) is int
-
-    if check_recette(id_recette):
-        global current_user
-
-        connexion = sqlite3.connect('websiteDB.db')
-        c = connexion.cursor()
-
-        request = f"""
-        INSERT INTO favoris
-        values ("{current_user}", {id_recette});
+    def give(self, table, nom):
         """
+        Fonction qui permet de faire des requêtes sql à une base de donnée quelconque.
+        Prend en paramètre :
+            -Le nom de la table
+            -Un ou des attributs de la table
+        Renvoie :
+            -Une liste avec les données demandées
+        """
+        self.c.execute(f"SELECT {nom} FROM {table};")
+        return self.c.fetchall()
 
-        print(request)
+    # fonction spécifique
+    def get_recettes(self, table, element, recette):
+        """
+        Fonction qui permet d'obtenir toutes les recettes d'une catégorie
+        """
+        self.c.execute(f"SELECT {element} FROM {table} WHERE categories LIKE '%{recette}%';")
+        return self.c.fetchall()
 
-        c.execute(request)
+    def get_recette_info(self, recette_id):
+        """
+        Fonction qui renvoie toutes les informations sur une recette
+        """
+        self.c.execute(f"SELECT * FROM recettes WHERE id = {recette_id}")
+        return self.c.fetchall()[0]
 
-        connexion.commit()
+    def get(self, attribut, table,condition1,  condition2):
+        """
+        Fonction qui permet d'acceder à la base de données en fonction d'une condition qui dépend de l'attribut
+        :param attribut:
+        :param table:
+        :param condition:
+        :return:
+        """
+        self.c.execute(f"SELECT {attribut} FROM {table} WHERE {condition1} = {condition2}")
+        return self.c.fetchall()
 
-        c.close()
-
-
-def add_comment(recette_id, note, comment):
-    """
-    Fonction qui permet d'ajouter un commentaire à la table comments
-
-    Retourne :
-        Rien
-    Pré-conditions :
-        recette_id est l'id d'une recette dans la DB
-        note est un integer compris entre 0 et 5
-        comment est une string qui contient le commentaire
-    Post-conditions :
-        un commentaire est ajouté à la table comments
-    """
-    assert check_recette(recette_id), "recette inconnue"
-    assert type(note) is int and 0 <= note <= 5, "note impossible"
-    assert type(comment) is str, "Mauvais format de commentaire"
-
-    global current_user
-
-    connexion = sqlite3.connect('websiteDB.db')
-    c = connexion.cursor()
-
-    c.execute(f"""
-    INSERT INTO comments
-    values (?, ?, ?, ?);
-    """, (current_user, recette_id, note, comment))
-
-    connexion.commit()
-
-    c.close()
-
-# ------ Fonctions de modification de la DB ------ #
-
-
-# ------ Tests ------ #
-"""
-add_user("test", "test", "test.test@gmail.com")
-add_recette("Pâtes au beurre", "pâtes;eau;beurre", "faire bouillir de l'eau;mettre les pâtes;ajouter un peu de beurre quand le tout est cuit", "Pâtes", "./img_recettes/pates_au_beurre.jpg", "test.test@gmail.com")
-add_recette("Pâtes à l'huile", "pâtes;eau;huile", "faire bouillir de l'eau;faire cuire les pâtes;ajouter un peu d'huile avant de servir", "Pâtes", "./img_recettes/pates_huile.jpg", "test.test@gmail.com")
-add_user("Alex", "Alex", "alex.bonjour@gmail.com")
-set_current_user("alex.bonjour@gmail.com")
-add_comment(0, 4, "Classique mais ça fait toujours plaisir !")
-add_favori(1)
-"""
+    # fonction login(email, mdp)
+    def login(self, email, mdp):
+        """
+        La fonction permet de vérifier si un utilisateur à rentrer le bon email et le bon mot de passe.
+        Prend en paramètre :
+            - email : email rentré par l'utilisateur
+            - mdp : mot de passe rentré par l'utilisateur
+        Renvoie :
+            - un boolen : Vrai si tout correspond False sinon
+        """
+        if check_user(email):
+            self.c.execute(f"SELECT mdp FROM users WHERE email LIKE '%{email}%';")
+            mdp_hash = self.c.fetchall()[0][0]
+            # print(mdp_hash)
+            ph = argon2.PasswordHasher()
+            if ph.verify(hash=mdp_hash, password=mdp):
+                self.c.execute(f"SELECT dark_mode FROM user_settings WHERE email LIKE '%{email}%';")
+                set_current_user(email)
+                if self.c.fetchall()[0][0] == 0:
+                    return True, False
 
 
+request = Request('websiteDB.db')
 
+# print(request.give('recettes', 'nom, image'))
+# print(request.get_recette('recettes', 'nom', 'pâtes au beurre'))
+# print(request.login('test.test@gmail.com', 'test'))
+# print(request.get_recettes("recettes", "nom, id", "Pâtes"))
